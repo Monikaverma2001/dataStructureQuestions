edbd21a8b0d627863bfe0ef0bf591b3b23e2ece8
monika verma <monika8427084@gmail.com>
2023-02-27 18:34:26 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    // Your code here
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        boolean[] slot = new boolean[n];
+        Arrays.sort(arr,(Job j1,Job j2)->(j2.profit-j1.profit));
+        int ans = 0;
+        int jobcount = 0;
+        for(Job job : arr){
+            int deadline = job.deadline;
+            while(deadline>=1){
+                if(slot[deadline-1]==false){
+                    slot[deadline-1]=true;
+                    ans+=job.profit;
+                    jobcount++;
+                    break;
+                }
+                deadline--;
+            }
+        }
+        return new int[]{jobcount,ans};
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
