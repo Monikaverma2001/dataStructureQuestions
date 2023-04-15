becc40ea4450bfe8bba1d6dd8152a3a89b359dbf
monika verma <monika8427084@gmail.com>
2023-04-15 21:32:54 +0530

    Added solution - LeetHub

---
+++ Pairs with certain difference - GFG/pairs-with-certain-difference.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[N];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int K = Integer.parseInt(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxSumPairWithDifferenceLessThanK(arr, N, K));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution { 
+    public static int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K) 
+    {
+        // Your code goes here 
+         Arrays.sort(arr);
+        int sum=0;
+        for(int i=N-1;i>0;)
+        {
+            if(Math.abs(arr[i]-arr[i-1])<K)
+            {
+                sum+=arr[i]+arr[i-1];
+                i-=2;
+            }
+            else
+                i-=1;
+        }
+        return sum;
+    }
+    
+}
