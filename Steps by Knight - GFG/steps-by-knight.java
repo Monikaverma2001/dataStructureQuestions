ef25e2c37bb4c7729277a9c7d4c00b175165d910
monika verma <monika8427084@gmail.com>
2023-04-24 20:36:20 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.java
+++ Steps by Knight - GFG/steps-by-knight.java
@@ -28,141 +28,86 @@ class GFG
 
 // } Driver Code Ends
 
-class Pair {
-    int row;
-    int col;
-    int time;
-    public Pair(int row, int col, int time) {
-        this.row = row;
-        this.col = col;
-        this.time = time;
-    }
-}
-class Solution
-{   
 
-    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
-    {   
-        boolean visited[][] = new boolean[N][N];
-        int chess[][] = new int[N][N];
-        int initrow = KnightPos[0]-1;
-        int initcol = KnightPos[1]-1;
-        int tarrow = TargetPos[0]-1;
-        int tarcol = TargetPos[1]-1;
-        
-        if(initrow == tarrow && initcol == tarcol) {
-            return 0;
+class Solution
+{
+    class pair{
+        int i,j,step;
+        pair(int sr,int sc,int s)
+        {
+            i=sr;
+            j=sc;
+            step=s;
         }
-        int res = Integer.MAX_VALUE;
+    }
+    //Function to find out minimum steps Knight needs to reach target position.
+    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
+    {
+        // Code here
+        Queue<pair> q=new ArrayDeque<>();
+        q.offer(new pair(KnightPos[0]-1,KnightPos[1]-1,0));
+        boolean[][] vis=new boolean[N][N];
+        vis[KnightPos[0]-1][KnightPos[1]-1]=true;
+        int min=Integer.MAX_VALUE;
         
-        int [] deltarow ={-2, -1, 1, 2, -2, -1, 1, 2};
-        int [] deltacol={-1, -2, -2, -1, 1, 2, 2, 1};
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(initrow, initcol, 0));
-        visited[initrow][initcol] = true;
-        while(q.isEmpty() == false) {
-            int row = q.peek().row;
-            int col = q.peek().col;
-            int time = q.peek().time;
-            if(row == tarrow && col == tarcol) {
-                res = Math.min(res, time);  
-            }
+        while(!q.isEmpty())
+        {
+            int i=q.peek().i;
+            int j=q.peek().j;
+            int step=q.peek().step;
+            
+             if(i==TargetPos[0]-1 && j==TargetPos[1]-1)
+                min=Math.min(min,step);
+                
             q.remove();
             
-            for(int i=0; i<8; i++) {
-                int nrow = row + deltarow[i];
-                int ncol = col + deltacol[i];
-                if(nrow>= 0 && nrow<N && ncol>= 0 && ncol<N && visited[nrow][ncol] == false) {
-                    visited[nrow][ncol] = true;
-                    q.add(new Pair(nrow, ncol, time+1));
-                }
+            if(i-2>=0 && j-1>=0 && vis[i-2][j-1]==false)
+            {
+                vis[i-2][j-1]=true;
+                q.offer(new pair(i-2,j-1,step+1));
             }
-        }
-        
-        return res;
-    }
-}
-
-// class Solution
-// {
-//     class pair{
-//         int i,j,step;
-//         pair(int sr,int sc,int s)
-//         {
-//             i=sr;
-//             j=sc;
-//             step=s;
-//         }
-//     }
-//     //Function to find out minimum steps Knight needs to reach target position.
-//     public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
-//     {
-//         // Code here
-//         Queue<pair> q=new ArrayDeque<>();
-//         q.offer(new pair(KnightPos[0]-1,KnightPos[1]-1,0));
-//         boolean[][] vis=new boolean[N][N];
-//         vis[KnightPos[0]-1][KnightPos[1]-1]=true;
-//         int min=Integer.MAX_VALUE;
-        
-//         while(!q.isEmpty())
-//         {
-//             int i=q.peek().i;
-//             int j=q.peek().j;
-//             int step=q.peek().step;
+            if(i-2>=0 && j+1<N && !vis[i-2][j+1])
+            {
+                vis[i-2][j+1]=true;
+                q.offer(new pair(i-2,j+1,step+1));
+            }
             
-//              if(i==TargetPos[0]-1 && j==TargetPos[1]-1)
-//                 min=Math.min(min,step);
-                
-//             q.remove();
+            if(j+2<N && i+1<N && !vis[i+1][j+2])
+            {
+                vis[i+1][j+2]=true;
+                q.offer(new pair(i+1,j+2,step+1));
+            }
+            if(j+2<N && i-1>=0 && ! vis[i-1][j+2])
+            {
+                vis[i-1][j+2]=true;
+                q.offer(new pair(i-1,j+2,step+1));
+            }
             
-//             if(i-2>=0 && j-1>=0 && vis[i-2][j-1]==false)
-//             {
-//                 vis[i-2][j-1]=true;
-//                 q.offer(new pair(i-2,j-1,step+1));
-//             }
-//             if(i-2>=0 && j+1<N && !vis[i-2][j+1])
-//             {
-//                 vis[i-2][j+1]=true;
-//                 q.offer(new pair(i-2,j+1,step+1));
-//             }
+            if(i+2<N && j+1<N && !vis[i+2][j+1])
+            {
+                vis[i+2][j+1]=true;
+                q.offer(new pair(i+2,j+1,step+1));
+            }
+            if(i+2<N && j-1>=0 && !vis[i+2][j-1])
+            {
+                vis[i+2][j-1]=true;
+                q.offer(new pair(i+2,j-1,step+1));
+            }
             
-//             if(j+2<N && i+1<N && !vis[j+2][i+1])
-//             {
-//                 vis[j+2][i+1]=true;
-//                 q.offer(new pair(j+2,i+1,step+1));
-//             }
-//             if(j+2<N && i-1>=0 && !vis[j+2][i-1])
-//             {
-//                 vis[j+2][i-1]=true;
-//                 q.offer(new pair(j+2,i-1,step+1));
-//             }
-            
-//             if(i+2<N && j+1<N && !vis[i+2][j+1])
-//             {
-//                 vis[i+2][j+1]=true;
-//                 q.offer(new pair(i+2,j+1,step+1));
-//             }
-//             if(i+2<N && j-1>=0 && !vis[i+2][j-1])
-//             {
-//                 vis[i+2][j-1]=true;
-//                 q.offer(new pair(i+2,j-1,step+1));
-//             }
-            
-//             if(j-2>=0 && i-1>=0 && !vis[j-2][i-1])
-//             {
-//                 vis[j-2][i-1]=true;
-//                 q.offer(new pair(j-2,i-1,step+1));
-//             }
-//             if(j-2>=0&& i+1<N && !vis[j-2][i+1])
-//             {
-//                 vis[j-2][i+1]=true;
-//                 q.offer(new pair(j-2,i+1,step+1));
-//             }
+            if(j-2>=0 && i-1>=0 && ! vis[i-1][j-2])
+            {
+                vis[i-1][j-2]=true;
+                q.offer(new pair(i-1,j-2,step+1));
+            }
+            if(j-2>=0&& i+1<N && !vis[i+1][j-2])
+            {
+                vis[i+1][j-2]=true;
+                q.offer(new pair(i+1,j-2,step+1));
+            }
             
             
              
-//         }
-//         return min;
-//     }
-// }
+        }
+        return min;
+    }
+}
