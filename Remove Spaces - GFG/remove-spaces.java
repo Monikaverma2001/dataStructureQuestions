ce341e0502e6809b81859a7b33a85b58beecfc53
Monika Verma <monika8427084@gmail.com>
2023-06-05 21:59:53 +0530

    Added solution - LeetHub

---
+++ Remove Spaces - GFG/remove-spaces.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0)
+        {
+            String s;
+            s = br.readLine();
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.modify(s));    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+   
+    String modify(String S)
+    {
+        // your code here
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<S.length();i++)
+        {
+            if(S.charAt(i)==' ')
+                continue;
+            str.append(S.charAt(i));
+        }
+        return str.toString();
+    }
+}
