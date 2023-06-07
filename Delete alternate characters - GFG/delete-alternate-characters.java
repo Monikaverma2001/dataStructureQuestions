fa73b03f125c308c4dbe5421e1e829f1ea94bf39
Monika Verma <monika8427084@gmail.com>
2023-06-07 17:40:20 +0530

    Added solution - LeetHub

---
+++ Delete alternate characters - GFG/delete-alternate-characters.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.delAlternate(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+
+class Solution {
+    static String delAlternate(String S) {
+        // code here
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<S.length();i++)
+        {
+            if(i%2==0)
+            {
+                str.append(S.charAt(i));
+            }
+        }
+        return str.toString();
+    }
+}
