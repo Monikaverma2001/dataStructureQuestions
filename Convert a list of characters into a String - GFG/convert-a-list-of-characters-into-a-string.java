8ce25d2eb7c21dd890671d442407ff107723ff67
Monika Verma <monika8427084@gmail.com>
2023-06-07 18:02:47 +0530

    Added solution - LeetHub

---
+++ Convert a list of characters into a String - GFG/convert-a-list-of-characters-into-a-string.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            int n = Integer.parseInt(read.readLine().trim());
+            
+            String input[] = read.readLine().trim().split(" ");
+            
+            char arr[] = new char[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = input[i].charAt(0);
+            }
+            
+            
+            Solution ob = new Solution();
+            String ans = ob.chartostr(arr, n);
+            
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public String chartostr(char arr[], int N){
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<N;i++)
+        {
+            str.append(arr[i]);
+        }
+        return str.toString();
+    }
+}
