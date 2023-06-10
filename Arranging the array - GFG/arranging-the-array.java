50a9b3e7e6dcd141c0999fe4efd392de0b4528a4
Monika Verma <monika8427084@gmail.com>
2023-06-10 09:10:45 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int a[], int n)
+    {
+        // Your code goes here
+        Map<Integer,Integer> map=new HashMap<>();
+        //Map<Integer,Integer> pos=new HashMap<>();
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<0)
+            {  
+                map.put(k,a[i]);
+                 k++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=0)
+               { map.put(k,a[i]);k++;}
+        }
+        for(int i=0;i<n;i++)
+        {
+            a[i]= map.get(i);
+        }
+    }
+}
