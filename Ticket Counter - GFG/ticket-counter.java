d25bda17ee6323c391c2ae88470d36ed0c4a31d0
Monika Verma <monika8427084@gmail.com>
2023-06-18 22:49:46 +0530

    Added solution - LeetHub

---
+++ Ticket Counter - GFG/ticket-counter.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s[]=in.readLine().trim().split(" ");
+            int n=Integer.parseInt(s[0]);
+            int k=Integer.parseInt(s[1]);
+            Solution ob=new Solution();
+            out.println(ob.distributeTicket(n,k));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public static int distributeTicket(int N,int K)
+    {
+        int l = 1, r = N;
+        while(r-l>=K){
+            l+=K;
+            if(r-l<K)return l;
+            r-=K;
+        }
+        return r;
+    }
+}
